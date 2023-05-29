436f99db263db3f3a92cc01dd9b8aaaa0891f1b9
Laia <laiafreixas9@gmail.com>
2023-05-29 20:18:17 +0200

    use open-elevation api instead of google maps

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # WhenWillIDrown
 A dumb tool to check what year will the sea level rise reach your location
+
+## Usage
+
+```
+python main.py 42.342198 3.203303
+
+```
+
+## Configure your API key
--- src/drown_time.py
+++ src/drown_time.py
@@ -3,6 +3,7 @@ import location
 
 def drown_time(longitude, latitude):
     elevation_m = location.elevation(longitude, latitude)
+    print(f"Elevation is {elevation_m}")
     elevation_mm = elevation_m*1000
     year = year_sea_level_will_reach(elevation_mm)
     return round(year)
